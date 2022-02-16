82820b05336459f4da117c5506106417271fc003
Fégan Pierre <fegan.pierre@gmail.com>
2022-02-16 16:34:01 +0100

    Add employee list component, edit employee form

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EMPLOYEES } from '../mock-employees';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  employees: Employee[] = EMPLOYEES 
+  selectedEmployee?: Employee
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(employee: Employee): void {
+    this.selectedEmployee = 
+      this.selectedEmployee == employee
+      ? undefined
+      : employee
+  }
+}
---
+++ src/app/mock-employees.ts
@@ -0,0 +1,15 @@
+import { Employee } from "./employee";
+
+export const EMPLOYEES: Employee[] = [
+  { id: 1, firstname: 'Jane', lastname: 'Doe' },
+  { id: 2, firstname: 'John', lastname: 'Doe' },
+  { id: 3, firstname: 'Marcus', lastname: 'Bobas' },
+  { id: 4, firstname: 'Carlos', lastname: 'Maneti' },
+  { id: 5, firstname: 'George', lastname: 'Domoas' },
+  { id: 6, firstname: 'Diane', lastname: 'Irresta' },
+  { id: 7, firstname: 'Melissa', lastname: 'Trevert' },
+  { id: 8, firstname: 'Kristen', lastname: 'Hierba' },
+  { id: 9, firstname: 'Gustave', lastname: 'Zamann' },
+  { id: 10, firstname: 'Cyril', lastname: 'Moretto' },
+  { id: 11, firstname: 'Brian', lastname: 'Tirano' }
+]
