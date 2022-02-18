7149eec323572ae4a3ef71a3d81263c7f6d38892
Fégan Pierre <fegan.pierre@gmail.com>
2022-02-18 17:10:17 +0100

    Add routing component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { EmployeesComponent } from './employees/employees.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeesComponent }
+]
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
